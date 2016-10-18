dc8948b9d859247aebcaba5343ebfd25cc89a2fc
Xavier Fernandez <xavier.fernandez@polyconseil.fr>
2016-10-18 10:22:47 +0200

    WIP2

--- pip_similarities.py
+++ pip_similarities.py
@@ -1,10 +1,12 @@
 from collections import defaultdict
+import itertools
+import json
 import zlib
 
 import utils
 
 K = 9
-SAMPLE_SIZE = 2000
+SAMPLE_SIZE = 5000
 
 
 def iter_compress_data(data):
@@ -16,13 +18,13 @@ def iter_compress_data(data):
         if len(text) < K:
             print('Ignoring issue %s' % number)
             continue
-        yield number, text.replace('\n', ' ')
+        yield number, text.replace('\n', ' '), item['closed_at'] is None
 
 
 def get_shingles(text):
     shingles = set()
     for i in range(0, len(text) - K + 1):
-        #shingles.add(text[i:i + K])
+        # shingles.add(text[i:i + K])
         shingles.add(zlib.adler32(text[i:i + K].encode()) & 0xffffffff)
     return shingles
 
@@ -31,18 +33,41 @@ def similarity(shingle1, shingle2):
     return len(shingle1 & shingle2) / len(shingle1 | shingle2)
 
 
-if __name__ == '__main__':
-    data = utils.load_data('./build/pip_issues.data')
+def compute_similarities(issue_data_filename):
+    data = utils.load_data(issue_data_filename)
     shingles = defaultdict(set)
-    for number, text in iter_compress_data(data):
+    opened_issues = {}
+    for number, text, opened in iter_compress_data(data):
         shingles[number] = get_shingles(text)
-    similarities = {}
-    for number1 in sorted(shingles):
-        for number2 in sorted(shingles):
-            if number2 <= number1:
-                continue
-            similarities[(number1, number2)] = similarity(shingles[number1], shingles[number2])
-    ordered_similarities = sorted((s, n) for n, s in similarities.items())
-    print(ordered_similarities[:10])
+        opened_issues[number] = opened
+    similarities = defaultdict(dict)
+    for number1, number2 in itertools.combinations(shingles, 2):
+        if not (opened_issues[number1] or opened_issues[number2]):
+            # Don't compute similarities if both are closed
+            continue
+        similarities[min(number1, number2)][max(number1, number2)] = similarity(shingles[number1], shingles[number2])
+
+    with open('build/similarities.data', 'w') as s:
+        json.dump(similarities, s)
+
+
+def get_similar_issues(symmetric, issue, threshold=0.10):
+    similar_issues = sorted([
+        (s, n)
+        for n, s in symmetric[issue].items()
+        if s > threshold
+    ], reverse=True)
+    return similar_issues
+
+if __name__ == '__main__':
+    with open('build/similarities.data') as s:
+        similarities = json.load(s)
+
+    symmetric = defaultdict(dict)
+    for number1 in similarities:
+        for number2 in similarities[number1]:
+            symmetric[number1][number2] = similarities[number1][number2]
+            symmetric[number2][number1] = similarities[number1][number2]
+    get_similar_issues(symmetric, '1234')
     import ipdb
     ipdb.set_trace()
