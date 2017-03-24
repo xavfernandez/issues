f10656c953c26a3a2bbf1591e8c5754d1b9f4a8f
Xavier Fernandez <xavier.fernandez@polyconseil.fr>
2017-03-24 20:06:37 +0100

    Ease the production of Open only graphs

--- load_pip_issues.py
+++ load_pip_issues.py
@@ -2,25 +2,25 @@ from collections import Counter
 
 import matplotlib.pyplot as plt
 
-from . import utils
+import utils
 
 
 global MIN_CREATED_AT
 
 
-def creation_week(item):
-    return (item['created_at'] - MIN_CREATED_AT).days // 7
+def creation_week(item, min_created_at):
+    return (item['created_at'] - min_created_at).days // 7
 
 
-def close_week(item):
+def close_week(item, min_created_at):
     if item['closed_at'] is None:
         return
-    return (item['closed_at'] - MIN_CREATED_AT).days // 7
+    return (item['closed_at'] - min_created_at).days // 7
 
 
-def output_close_speed_by_week(items):
-    created_by_week = Counter(creation_week(item) for item in items)
-    closed_by_week = Counter(close_week(item) for item in items)
+def output_close_speed_by_week(items, min_created_at):
+    created_by_week = Counter(creation_week(item, min_created_at) for item in items)
+    closed_by_week = Counter(close_week(item, min_created_at) for item in items)
 
     max_week = max(max(created_by_week.keys(), closed_by_week.keys()))
     still_opened = 0
@@ -36,19 +36,23 @@ def output_close_speed_by_week(items):
 
 
 def plot(items, item_type):
-    result = output_close_speed_by_week(items)
+    result = output_close_speed_by_week(items, MIN_CREATED_AT)
     weeks = [i[0] for i in result]
     # total_opened = [i[1] for i in result]
     total_closed = [i[2] for i in result]
     still_opened = [i[3] for i in result]
 
     plt.figure()
-    plt.title('pip - Open vs closed %s' % item_type.capitalize())
     plt.xlabel('Weeks')
     plt.ylabel('Number of %s' % item_type)
 
-    plt.bar(weeks, total_closed, color='b')
-    plt.bar(weeks, still_opened, color='r', bottom=total_closed)
+    if False:
+        plt.title('pip - Open vs closed %s' % item_type.capitalize())
+        plt.bar(weeks, total_closed, color='b')
+        plt.bar(weeks, still_opened, color='r', bottom=total_closed)
+    else:
+        plt.title('pip - Open %s' % item_type.capitalize())
+        plt.bar(weeks, still_opened, color='r')
     plt.show()
 
 
